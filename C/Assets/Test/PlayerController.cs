91adb538ec2f9a92fc74d32c5158cbeea7255ba7
JingWen <jingwenhuang1008@gmail.com>
2024-08-24 15:30:59 +0800

    add: 簡易測試玩家控制器

---
+++ C/Assets/Test/PlayerController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        //isGrounded = Physics2D.OverlapCircle(transform.position, 0.1f, groundLayer);
+        print(isGrounded);
+        if (isGrounded && Input.GetKeyDown(KeyCode.W))
+        {
+            print("awdadw");
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Map"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Map"))
+        {
+            isGrounded = false;
+        }
+    }
+}
