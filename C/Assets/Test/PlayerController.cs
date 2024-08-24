8f672d40a19342915837c79af121056d10fbcb5f
33 <whu950701@gmail.com>
2024-08-24 19:44:22 +0800

    可以連續跳躍

--- C/Assets/Test/PlayerController.cs
+++ C/Assets/Test/PlayerController.cs
@@ -1,4 +1,4 @@
-/*using UnityEngine;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private bool isGrounded;
 
+    public int jumpCoda = 0;
+    public bool canResetJumpCoda = true;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -18,6 +20,7 @@ public class PlayerController : MonoBehaviour
     {
         Move();
         Jump();
+        print(jumpCoda+"-----"+canResetJumpCoda);
     }
 
     void Move()
@@ -29,19 +32,31 @@ public class PlayerController : MonoBehaviour
     void Jump()
     {
         //isGrounded = Physics2D.OverlapCircle(transform.position, 0.1f, groundLayer);
-        if (isGrounded && Input.GetKeyDown(KeyCode.W))
+        if (jumpCoda>0 && Input.GetKeyDown(KeyCode.W))
         {
-            print("awdadw");
+            jumpCoda--;
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        print(collision.gameObject);
         if (collision.gameObject.layer == LayerMask.NameToLayer("Map"))
         {
+
+            jumpCoda = 1;
             isGrounded = true;
         }
+        if (collision.gameObject.layer == LayerMask.NameToLayer("EnemyHitBox"))
+        {
+            if(canResetJumpCoda)
+            {
+                jumpCoda ++;
+                canResetJumpCoda = false;
+            }
+        }
+
     }
 
     void OnCollisionExit2D(Collision2D collision)
@@ -50,119 +65,13 @@ public class PlayerController : MonoBehaviour
         {
             isGrounded = false;
         }
-    }
-}
-*/
-using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    Rigidbody2D rb;
-    Animator anim;
-    bool isTouchingWall = false;
-    bool isWalking = false;
-    bool isJumping = false;
-    bool canJump = true;
-    bool canControl = false; // 新添加的变量
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-    }
-
-    void Update()
-    {
-        bool keydn = Input.anyKey;
-
-        if (!keydn)
+        if (collision.gameObject.layer == LayerMask.NameToLayer("EnemyHitBox"))
         {
-            anim.SetBool("mov", false);
-            anim.SetBool("jump", false);
-        }
 
-        if (!Input.GetKey(KeyCode.W) && !canJump)
-        {
-            canJump = true;
-        }
-
-        if (Input.GetKeyDown(KeyCode.W) && canJump && canControl) // 添加了canControl条件
-        {
-            anim.SetBool("jump", true);
-            JumpCharacter(); // 跳躍
-        }
-
-        if (Input.GetKey(KeyCode.A) && canControl) // 添加了canControl条件
-        {
-            anim.SetBool("mov", true);
-            MoveCharacter(-1f); // 向左移動
-            FlipCharacter(true); // 水平翻轉
-        }
-
-        if (Input.GetKey(KeyCode.D) && canControl) // 添加了canControl条件
-        {
-            anim.SetBool("mov", true);
-            MoveCharacter(1f); // 向右移動
-            FlipCharacter(false); // 恢復水平方向
+            canResetJumpCoda = true;
         }
     }
 
-    void MoveCharacter(float direction)
-    {
-        Vector2 targetVelocity = new Vector2(direction * 3f, rb.velocity.y);
-        rb.velocity = targetVelocity;
-        isWalking = true;
-    }
-
-    void JumpCharacter()
-    {
-        if (IsGrounded())
-        {
-            rb.velocity = new Vector2(rb.velocity.x, 16f);
-            canJump = false;
-            isJumping = true;
-        }
-    }
-
-    bool IsGrounded()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f);
-        return hit.collider != null;
-    }
-
-    void FlipCharacter(bool flip)
-    {
-        if (flip)
-        {
-            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-        }
-        else
-        {
-            transform.rotation = Quaternion.identity;
-        }
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        {
-            isTouchingWall = true;
-            isWalking = false;
-            isJumping = false;
-        }
-
-
-        if (collision.gameObject.tag == "Ground")
-        {
-            anim.SetBool("jump", false);
-            canControl = true; // 当接触地面时，启用控制
-        }
-    }
-
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-            canControl = false; // 当离开地面时，禁用控制
-        }
-    }
+    
 }
